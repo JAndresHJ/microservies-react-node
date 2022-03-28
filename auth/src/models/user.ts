f01e7dce7046e49c5c264ea4533eb7725a2bfcd5
Andres <jandreshdzj@gmail.com>
2022-03-28 16:33:32 -0600

    Authentication Strategies and Optios

---
+++ auth/src/models/user.ts
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+import { Password } from "../services/password";
+
+// An interface that describes the properties
+// that are requierd to create a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describe properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      }
+    }
+  }
+);
+
+// Whener a user is inteded to be saved in mongo that save attempt 
+// is intercepted by this hook. It takes the user password that is set
+// on the User Document, hash it and overwrite the password on the
+// document. Useful to implement an email change functionality
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
